d7dd7fb207fc5c6207969e85afef6f00d76c2d98
AliLordLoss <a.a.ghanati@gmail.com>
2023-04-14 21:45:54 +0330

    - added tests to improve coverage

--- src/test/java/org/json/simple/TestJson.java
+++ src/test/java/org/json/simple/TestJson.java
@@ -4,34 +4,63 @@
  */
 package org.json.simple;
 
-import static org.junit.Assert.assertEquals;
+import org.json.simple.parser.ParseException;
+import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class TestJson {
 
-	@Test
-	public void testDecode() throws Exception{
-		System.out.println("=======decode=======");
+    @Test
+    public void testDecode() throws Exception {
+        System.out.println("=======decode=======");
 
-		String s="[0,10]";
-		Object obj=JSONValue.parse(s);
-		JSONArray array=(JSONArray)obj;
-		System.out.println("======the 2nd element of array======");
-		System.out.println(array.get(1));
-		System.out.println();
-		assertEquals("10",array.get(1).toString());
-	}
-	
+        String s = "[0,10]";
+        Object obj = JSONValue.parse(s);
+        JSONArray array = (JSONArray) obj;
+        System.out.println("======the 2nd element of array======");
+        System.out.println(array.get(1));
+        System.out.println();
+        assertEquals("10", array.get(1).toString());
+    }
+
+    @Test
+    public void testJSONArrayCollection() {
+        final ArrayList<String> testList = new ArrayList<String>();
+        testList.add("First item");
+        testList.add("Second item");
+        final JSONArray jsonArray = new JSONArray(testList);
+
+        assertEquals("[\"First item\",\"Second item\"]", jsonArray.toJSONString());
+    }
+
+    @Test
+    public void testJSONObject() {
+        final HashMap<String, String> obj = new HashMap<String, String>();
+        obj.put("first-key", "first-value");
+        obj.put("second-key", "second-value");
+        final JSONObject jsonObject = new JSONObject(obj);
+
+        assertEquals("{\"first-key\":\"first-value\",\"second-key\":\"second-value\"}", jsonObject.toJSONString());
+    }
+
+    @Test(expected = ParseException.class)
+    public void testParserWithException() throws ParseException {
+            String jsonString = "[\"one\",2,\"three\",4";
+            JSONValue.parseWithException(jsonString);
+    }
+
 	@Test
-	public void testJSONArrayCollection() {
-		final ArrayList<String> testList = new ArrayList<String>();
-		testList.add("First item");
-		testList.add("Second item");
-		final JSONArray jsonArray = new JSONArray(testList);
-		
-		assertEquals("[\"First item\",\"Second item\"]", jsonArray.toJSONString());
+	public void testJSONArrayBoolean() {
+		final boolean[] testList = new boolean[3];
+		testList[0] = true;
+		testList[1] = true;
+		final JSONArray jsonArray = new JSONArray(Collections.singleton(testList));
+
+		assertEquals("[[true,true,false]]", jsonArray.toString());
 	}
 }
